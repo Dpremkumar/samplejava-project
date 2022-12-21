3a8c3281555acf5690f865e605db9da5a967a542
Dpremkumar <34860411+Dpremkumar@users.noreply.github.com>
2022-12-21 18:59:59 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Write your code here
-      system.out.println("Hello, World");
-    }
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
